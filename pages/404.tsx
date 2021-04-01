8da2a8b53cebfe49eff47aa1ad422f47b840eee8
Leonard Laput <online.lorno@gmail.com>
2021-04-01 22:02:39 +0800

    feat: error pages

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import Container from 'components/Container'
+
+const IndexPage: React.FC = () => (
+  <>
+    <Head>
+      <title>Bisita - Foot traffic tracker</title>
+    </Head>
+    <Container>
+      <h1>404 - Page Not Found</h1>
+    </Container>
+  </>
+)
+
+export default IndexPage
---
+++ pages/500.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import Container from 'components/Container'
+
+const IndexPage: React.FC = () => (
+  <>
+    <Head>
+      <title>Bisita - Foot traffic tracker</title>
+    </Head>
+    <Container>
+      <h1>500 - Server-side error occurred</h1>
+    </Container>
+  </>
+)
+
+export default IndexPage
