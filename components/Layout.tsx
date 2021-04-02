3dce4f68f5569e0d345bd7e7b285be68ed466b20
Leonard Laput <online.lorno@gmail.com>
2021-04-02 21:43:42 +0800

    refactor: add min-height to content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import Head from 'next/head'
+import styled from 'styled-components'
 
 import Header from 'components/Header'
 import Footer from 'components/Footer'
@@ -8,13 +9,17 @@ type Props = {
   children?: ReactNode
 }
 
+const Content = styled.div`
+  min-height: 100vh;
+`
+
 const Layout: React.FC<Props> = ({ children }) => (
   <>
     <Head>
       <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
     <Header />
-    {children}
+    <Content>{children}</Content>
     <Footer />
   </>
 )
